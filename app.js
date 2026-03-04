62c6c10255056efc458e259c9801a098cdef6277
Sagar <sagarsurya45@gmail.com>
2026-03-04 12:48:14 +0530

    admin routes

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+
+dotenv.config(); // ← FIRST
+
 import dbConnection from "./src/config/db.js";
+import userRoutes from "./src/user/user.routes.js";
+import adminRoutes from "./src/admin/admin.router.js";
 
 const app = express();
 
-dotenv.config();
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// DB
 dbConnection();
 
-app.get("/api", (req, res) => {
+// Routes
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/admin", adminRoutes)
+
+app.get("/", (req, res) => {
   res.json("Doc_Gen_Backend");
 });
 
