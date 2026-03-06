5c214584f0d8286dd2d4aeb081b410cfbed78810
Sagar <sagarsurya45@gmail.com>
2026-03-06 17:06:40 +0530

    define & test appointmentLetter api

--- app.js
+++ app.js
@@ -2,14 +2,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import dbConnection from "./src/config/db.js";
-import appointmentRoutes from "./src/modeuls/documents/documentRoutes/appointmentLetterRoutes.js";
 import userRoutes from "./src/user/user.routes.js";
 import adminRoutes from "./src/admin/admin.router.js";
-import offerLetterRoutes from "./src/modeuls/documents/documentRoutes/offerLetterRoutes.js";
-import experienceLetterRoutes from "./src/modeuls/documents/documentRoutes/experienceLetterRoutes.js";
-import fullAndFinalRoutes from "./src/modeuls/documents/documentRoutes/fullAndFinalRoutes.js";
-import confirmationLetterRoutes from "./src/modeuls/documents/documentRoutes/confirmationLetterRoutes.js";
-import completionCertificateRoutes from "./src/modeuls/documents/documentRoutes/completionCertificateRoutes.js";
+import documentsRoutes from "./src/modeuls/documents/documentRoutes/documentsRoutes.js"
 const app = express();
 
 app.use(express.json());
@@ -18,19 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 dotenv.config();
 dbConnection();
 
-/* ================= ROUTES ================= */
-app.use("/api/v1/appointment-letter", appointmentRoutes);
-app.use("/api/v1/offer-letter", offerLetterRoutes);
-app.use("/api/v1/experience-letter", experienceLetterRoutes);
-app.use("/api/v1/fnf", fullAndFinalRoutes);
-app.use("/api/confirmation-letter", confirmationLetterRoutes);
-app.use("/api/v1/completion-certificate", completionCertificateRoutes);
-
 // Routes
-
-
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/admin", adminRoutes)
+app.use("/api/v1/documents", documentsRoutes);
 
 app.get("/", (req, res) => {
   res.json("Doc_Gen_Backend");
---
+++ src/modeuls/documents/documentRoutes/documentsRoutes.js
@@ -0,0 +1,26 @@
+import express from "express"
+import { createAppointmentLetter, getAllAppointmentLetters, getAppointmentLetterById,updateAppointmentLetter, deleteAppointmentLetter } from "../documentController/appointmentLetterController.js"
+
+
+
+const router = express.Router()
+
+// generate documents routes
+router.post("/generate_appointment_letter", createAppointmentLetter) // Appintment Letter
+
+
+// get documents routes
+router.get("/appointment_letters", getAllAppointmentLetters)
+router.get("/appointment_letter/:id",getAppointmentLetterById)
+
+
+// update documents routes
+router.put("/appointment_letter/:id", updateAppointmentLetter);
+
+
+
+// delete documents routes
+router.delete("/delete_appointment_letter/:id",deleteAppointmentLetter);
+
+
+export default router;
