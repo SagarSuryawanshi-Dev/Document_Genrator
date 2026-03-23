4edd7891b11726a4fee5b68fb244e56c54e04419
Sagar <sagarsurya45@gmail.com>
2026-03-23 12:28:20 +0530

    issued by reslove

--- src/config/db.js
+++ src/config/db.js
@@ -4,6 +4,22 @@ const dbConnection = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log("Database connection Successfully");
+
+    const collection = mongoose.connection.collection("documents");
+
+    try {
+      await collection.dropIndex("company_1_employeeName_1_joiningDate_1");
+      console.log(" Dropped employeeName index");
+    } catch (err) {
+      console.log(" employeeName index not found");
+    }
+
+    try {
+      await collection.dropIndex("company_1_candidateName_1_joiningDate_1");
+      console.log(" Dropped candidateName index");
+    } catch (err) {
+      console.log(" candidateName index not found");
+    }
   } catch (error) {
     console.log("Database Connection Failed", error.message);
     process.exit(1);
