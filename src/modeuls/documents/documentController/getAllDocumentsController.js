a3f674553266acdac81f22b8ef960ce54c1a2f4a
PrathameshBhavsar-Dev <prathamesh69bhavsar@gmail.com>
2026-04-02 17:55:17 +0530

    made changes in the Get all docs controller

--- src/modeuls/documents/documentController/getAllDocumentsController.js
+++ src/modeuls/documents/documentController/getAllDocumentsController.js
@@ -1,5 +1,6 @@
-import  Document  from "../documentModel/BaseDocument.js";
- const getAllDocuments = async (req, res, next) => {
+import Document from "../documentModel/BaseDocument.js";
+
+const getAllDocuments = async (req, res, next) => {
   try {
     const { page = 1, limit = 10, docType } = req.query;
 
@@ -10,9 +11,15 @@ import  Document  from "../documentModel/BaseDocument.js";
     }
 
     const documents = await Document.find(query)
+      .populate("issuedBy", "name email")
       .skip((page - 1) * limit)
       .limit(Number(limit))
       .sort({ createdAt: -1 });
+
+    const formattedDocs = documents.map((doc) => ({
+      ...doc.toObject(),
+      issuedByName: doc.issuedBy?.name || null,
+    }));
 
     const total = await Document.countDocuments(query);
 
@@ -21,13 +28,11 @@ import  Document  from "../documentModel/BaseDocument.js";
       total,
       page: Number(page),
       pages: Math.ceil(total / limit),
-      data: documents,
+      data: formattedDocs,
     });
   } catch (error) {
     next(error);
   }
 };
-
-
 
 export default getAllDocuments;
